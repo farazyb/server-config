86c04b42246edb1304a0bfedf7d2b655be0fd145
faraz <farazyazdanib@gmail.com>
2021-10-08 02:18:56 +0330

    Initial commit

---
+++ src/test/java/com/fyb/serverconfig/ServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyb.serverconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
